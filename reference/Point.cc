ec6fd23805a4b1a5640ef54d38e6234227d73aed
Shengren Li <shengrenli12@gmail.com>
2011-10-15 23:23:23 -0700

    Remove files in 'reference'.

--- reference/Point.cc
+++
@@ -1,9 +0,0 @@
-
-#include "Point.h"
-#include <iostream>
-
-std::ostream& operator<<(std::ostream& out, const Point& p)
-{
-  out << '[' << p.x() << ", " << p.y() << ", " << p.z() << ']';
-  return out;
-}
